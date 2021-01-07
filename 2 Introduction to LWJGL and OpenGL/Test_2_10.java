d1e87be8d6a8c75184ae19df4ca2de102ea58837
Lee Stemkoski <stemkoski@gmail.com>
2021-01-07 14:27:59 -0500

    added input examples

---
+++ 2 Introduction to LWJGL and OpenGL/Test_2_10.java
@@ -0,0 +1,45 @@
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL40.*;
+
+import core.*;
+
+public class Test_2_10 extends Base
+{
+    public int programRef, vaoRef, vertexCount;
+    public Uniform translation, baseColor;
+
+    public void initialize()
+    {
+        System.out.println("Initializing...");
+
+    }
+
+    public void update()
+    {
+        /*
+        // debug printing
+        if (input.keyDownList.size() > 0)
+            System.out.println( "Keys down:" + input.keyDownList.toString() );
+        if (input.keyPressedList.size() > 0)
+            System.out.println( "Keys pressed:" + input.keyPressedList.toString() );
+        if (input.keyUpList.size() > 0)
+            System.out.println( "Keys up:" + input.keyUpList.toString() );
+        */
+
+        // typical usage
+        if (input.isKeyDown(GLFW_KEY_SPACE))
+            System.out.println( "The 'space' key was just pressed down.");
+        if (input.isKeyPressed(GLFW_KEY_RIGHT))
+            System.out.println( "The 'right arrow' key is currently pressed.");
+        if (input.isKeyUp(GLFW_KEY_A))
+            System.out.println( "The 'A' key was just released.");
+    
+    }
+
+    // driver method
+    public static void main(String[] args)
+    {
+        new Test_2_10().run();
+    }
+
+}
